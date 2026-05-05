0c3ca5a841beba375481b9b2c6772a119b391999
Kshitij Sharma <kshitij466e@gmail.com>
2026-05-05 23:05:28 +0530

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        sort(stones.begin(), stones.end());
+        int n = stones.size();
+        while (n > 1)
+        {
+            int x = stones[n-1]-stones[n-2];
+            n -= 2;
+            if (x > 0)
+            {
+                int l=0, r=n;
+                while (l < r)
+                {
+                    int mid = (l+(r-l)/2);
+                    if (stones[mid] < x)
+                    {
+                        l = mid+1;
+                    }
+                    else
+                    {
+                        r = mid;
+                    }
+                }
+                int pos = l;
+                stones.push_back(0);
+                for (int i= n+1; i>pos; i--)
+                {
+                    stones[i] = stones[i-1];
+                }
+                stones[pos] = x;
+                n++;
+            }
+        }
+        return (n>0 ? stones[0] : 0);
+    }
+};
