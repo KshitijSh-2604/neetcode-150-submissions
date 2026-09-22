84dcb8f2bde383d1e9b74cdf495ae546379badcb
Kshitij Sharma <kshitij466e@gmail.com>
2026-09-22 17:46:38 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cpp
@@ -0,0 +1,59 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool eOW;
+    TrieNode()
+    {
+        for (int i=0; i<26; i++)
+        {
+            children[i] = nullptr;
+        }
+        eOW = false;
+    }
+};
+class PrefixTree {
+    TrieNode* root;
+public:
+    PrefixTree() {
+        root = new TrieNode();
+    }
+    void insert(string word) {
+        TrieNode* cur = root;
+        for (char c : word)
+        {
+            int i = c-'a';
+            if (cur->children[i] == nullptr)
+            {
+                cur->children[i] = new TrieNode();
+            }
+            cur = cur->children[i];
+        }
+        cur->eOW = true;
+    }
+    bool search(string word) {
+        TrieNode* cur = root;
+        for (char c : word)
+        {
+            int i = c-'a';
+            if (cur->children[i] == nullptr)
+            {
+                return false;
+            }
+            cur = cur->children[i];
+        }
+        return cur->eOW;
+    }
+    bool startsWith(string prefix) {
+        TrieNode* cur = root;
+        for (char c : prefix)
+        {
+            int i = c-'a';
+            if (cur->children[i] == nullptr)
+            {
+                return false;
+            }
+            cur = cur->children[i];
+        }
+        return true;
+    }
+};
